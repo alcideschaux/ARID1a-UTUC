dbf0008a38bd7c95337489328950b9c5e34d002c
alcideschaux <drchaux@drchaux-AO722.(none)>
2014-04-29 08:51:39 -0400

    Updated R file

--- arid1a.utuc.R
+++ arid1a.utuc.R
@@ -3,4 +3,14 @@
 # Opening the dataset
 arid1a.full <- read.csv(file = file.choose())
 # Selecting variables relevant to the analysis
-data.selection <- c()
+arid1a.data<- arid1a.full[c("sex", "age", "location", "location2", "pT",
+                      "pT.2", "pT.a", "WHO.grade", "lymph.nodes", "lvi",
+                      "metastasis", "bladder.recur", "fu.recur",
+                      "tumor.prog", "fu.prog", "outcome", "dod",
+                      "fu.outcome", "tumor.h.med", "tumor.pos.med",
+                      "normal.h.med", "normal.pos.med", "tumor.h.0",
+                      "tumor.h.p50", "tumor.h.q3", "tumor.pos.0",
+                      "tumor.pos.p50", "tumor.pos.q3")]
+# Loading the libraries required for the analysis
+library(psych)
+library(gmodels)
