30ca6352d829b772b0e485936dc6a4614786abfc
alcideschaux <drchaux@drchaux-AO722.(none)>
2014-04-29 08:26:26 -0400

    Creating dataframes

---
+++ arid1a.utuc.R
@@ -0,0 +1,6 @@
+# ARID1a Expression in Upper Tract Urothelial Carcinoma
+# DATA ANALYSIS
+# Opening the dataset
+arid1a.full <- read.csv(file = file.choose())
+# Selecting variables relevant to the analysis
+data.selection <- c()
